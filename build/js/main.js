54eaf0cf89ded3d8ed220207198bf4f224ff4bfa
denchik2019 <53688435+denchik2019@users.noreply.github.com>
2020-08-23 21:16:06 +0300

    Add files via upload

--- build/js/main.js
+++ build/js/main.js
@@ -1,30 +1,39 @@
 'use strict';
 
-$('.product__item').on('click', function(e) {
+function onClick() {
+    
+        $(this).toggleClass('selected');
+        $(this).find('.product__ellipse').toggleClass('active-selected');
+        $(this).find('.product__title').toggleClass('active-selected');
+        
+        if($(this).hasClass('product__item--disable')) {
+            $(this).removeClass('selected');
+            $(this).find('.product__ellipse').removeClass('active-selected');
+            $(this).find('.product__title').removeClass('active-selected')
+        }
+    
+        if($(this).hasClass('selected')) {
+            $(this).removeClass('selected-hover');
+            $(this).find('.product__ellipse').removeClass('selected-hover');
+            $(this).find('.product__title').removeClass('selected-hover');
+            $(this).unbind('mouseleave', mouseleave);
+        }
+    
+        if(!$(this).hasClass('selected')) {
+            $(this).removeClass('selected-hover');
+            $(this).find('.product__ellipse').removeClass('selected-hover');
+            $(this).find('.product__title').removeClass('selected-hover');
+            $(this).hover('', mouseleave);
+        }
+    }
+
+
+$('.product__item').on('click', onClick);
+
+$('.product__link').on('click', function(e) {
     e.preventDefault();
-    $(this).toggleClass('selected');
-    $(this).find('.product__ellipse').toggleClass('active-selected');
-    $(this).find('.product__title').toggleClass('active-selected');
-
-    if($(this).hasClass('product__item--disable')) {
-        $(this).removeClass('selected');
-        $(this).find('.product__ellipse').removeClass('active-selected');
-        $(this).find('.product__title').removeClass('active-selected')
-    }
-
-    if($(this).hasClass('selected')) {
-        $(this).removeClass('selected-hover');
-        $(this).find('.product__ellipse').removeClass('selected-hover');
-        $(this).find('.product__title').removeClass('selected-hover');
-        $(this).unbind('mouseleave', mouseleave);
-    }
-
-    if(!$(this).hasClass('selected')) {
-        $(this).removeClass('selected-hover');
-        $(this).find('.product__ellipse').removeClass('selected-hover');
-        $(this).find('.product__title').removeClass('selected-hover');
-        $(this).hover('', mouseleave);
-    }
+    let elem = $(this).parent().parent().parent();
+    elem.find('.product__item').toggleClass('selected');
 });
 
 
@@ -32,7 +41,6 @@ function mouseleave() {
     $(this).not('.product__item--disable').addClass('selected-hover');
     $(this).find('.product__ellipse').not('.product__ellipse--disable').addClass('selected-hover');
     $(this).find('.product__title').not('.product__title--disable').addClass('selected-hover');
-
     if($(this).hasClass('selected-hover')) {
         $(this).removeClass('selected');
         $(this).find('.product__ellipse').removeClass('active-selected');
